e7c9e663c3f45a523ae48f630cb0cd3df896e336
sidra <sidrajagrawal@gmail.com>
2025-09-27 20:19:36 +0530

    DSA

---
+++ Day1/Merge_Sorted_Array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Merge_Sorted_Array{
+    class Solution {
+        public int findKthLargest(int[] nums, int k) {
+	    	PriorityQueue<Integer> hp = new PriorityQueue<>();
+	    	for(int i=0;i<k;i++) {
+	    		hp.add(nums[i]);
+	    	}
+	    	for(int i=k;i<nums.length;i++) {
+	    		if(hp.peek() < nums[i]) {
+	    			hp.poll();
+	    			hp.add(nums[i]);
+	    		}
+	    	}
+	    	return hp.peek();
+	    }
+}
+}
---
+++ Day1/Valid_Perfect_Square.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Valid_Perfect_Square{
+    class Solution {
+    public boolean isPerfectSquare(int num) {
+        long s=1,e=num;
+        boolean ans = false;
+        while(s<=e){
+            long m=s+(e-s)/2;
+            long sq= m*m;
+            if(sq > num){
+                 e=m-1;
+            }
+            else if(sq < num){
+                s=m+1;
+            }
+            else{
+                ans =true;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+}
